68ca65f771f98c2c813868d4212984266077e55f
GamingBeast1 <116881154+GamingBeast1@users.noreply.github.com>
2024-01-21 14:18:10 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -34,8 +34,7 @@ async def button(bot, update):
     elif update.data == "about":
         await update.message.edit(
             text=Translation.TECH_VJ_ABOUT_TEXT,
-            reply_markup=Translation.TECH_VJ_ABOUT_BUTTONS,
-            disable_web_page_preview=True
+            reply_markup=Translation.TECH_VJ_ABOUT_BUTTONS
         )
     elif "close" in update.data:
         await update.message.delete(True)
